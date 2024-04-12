54680f6010dde6aa0d39b4014b20845a72a97651
Winston Feng <tifish@gmail.com>
2024-04-12 11:55:17 +0800

    At lease build when PublishDirectory.txt not exist.

--- Publish.cmd
+++ Publish.cmd
@@ -1,6 +1,14 @@
 @echo off
 setlocal
 
+taskkill /f /im JeekNoteExplorer.exe >nul 2>nul
+dotnet build --configuration Release
+if errorlevel 1 (
+    echo Build failed.
+    pause
+    exit /b
+)
+
 if not exist PublishDirectory.txt (
     echo Please create a file named PublishDirectory.txt and write the target directory path in it.
     pause
@@ -9,10 +17,9 @@ if not exist PublishDirectory.txt (
 
 set /p targetDir=<PublishDirectory.txt
 
-taskkill /f /im JeekNoteExplorer.exe >nul 2>nul
-dotnet build --configuration Release &&^
 xcopy /e /y bin "%targetDir%\" &&^
 cd /d "%targetDir%" &&^
-start "" JeekNoteExplorer.exe
+start "" JeekNoteExplorer.exe ||^
+pause
 
 endlocal
